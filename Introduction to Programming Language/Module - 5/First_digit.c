818fe90052ddce8c70040eaf060c99b4e1bad685
shihab-mridha <shihabmridha001@gmail.com>
2026-06-13 11:38:46 +0600

    add new file

---
+++ Introduction to Programming Language/Module - 5/First_digit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+    int a;
+    int b;
+    scanf("%d",&a);
+    b = a/1000;
+    if(b%2==0){
+        printf("EVEN");
+    }
+    else{
+        printf("ODD");
+    }
+
+
+
+    
+
+}
