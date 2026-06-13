00e4f29dcf89386907e6148aa4f6156e000f4df8
shihab-mridha <shihabmridha001@gmail.com>
2026-06-13 11:13:12 +0600

    Fixed Module 5 submodule issue and added real files

---
+++ Introduction to Programming Language/Module - 5/conditonal_statement.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a;
+    int b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    if(a >= b){
+        printf("yes");
+    }
+    else{
+        printf("no");
+    }
+
+
+}
